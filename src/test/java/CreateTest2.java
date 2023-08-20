7359fd44aff9eca672e0cdcc0df2286f798e2946
vkzoomin996 <vkzoomin996@gmail.com>
2023-08-20 15:36:56 +0530

    this is my second commit

---
+++ src/test/java/CreateTest2.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class CreateTest2 {
+	@Test
+	public void createtest() {
+		System.out.println("----create test------");
+	}
+	@Test
+	public void deleteTest() {
+		System.out.println("----delete test------");
+	}
+
+}
