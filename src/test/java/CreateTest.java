60f7ad7ce549933c91a6cf66a22c10f861dbe34a
vkzoomin996 <118336463+vkzoomin996@users.noreply.github.com>
2023-08-20 16:02:20 +0530

    Update CreateTest.java

--- src/test/java/CreateTest.java
+++ src/test/java/CreateTest.java
@@ -1,13 +1,5 @@
 import org.testng.annotations.Test;
 
 public class CreateTest {
-	@Test
-	public void createtest() {
-		System.out.println("----create test------");
-	}
-	@Test
-	public void deleteTest() {
-		System.out.println("----delete test------");
-	}
-
+ 
 }
