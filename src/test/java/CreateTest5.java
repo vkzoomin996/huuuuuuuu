343d0b7241bc275f0d8d6726f7ff84ebcc5788f3
vkzoomin996 <vkzoomin996@gmail.com>
2023-08-20 15:54:51 +0530

    yavanige gottu

---
+++ src/test/java/CreateTest5.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class CreateTest5 {
+	@Test
+	public void createtest() {
+		System.out.println("----create test------");
+	}
+	@Test
+	public void deleteTest() {
+		System.out.println("----delete test------");
+	}
+
+}
